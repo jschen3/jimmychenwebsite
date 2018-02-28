132b250aa5bcd0d3e5cb6f390261ac186db04330
Prachi Bora <pbora@walmartlabs.com>
2018-02-27 18:32:16 -0800

    added expiry

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var createUser = require('./controllers/createUser');
 var createItem = require('./controllers/createItem');
+var getItemsByVoucherExpiry = require('./controllers/getItemsByExpiry');
+
 
 var mongoose = require('mongoose');
 var mongoDB = 'mongodb://127.0.0.1/test';
@@ -13,9 +15,7 @@ mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-/*app.get('/hello', function (req, res) {
-  res.send('Hello World!');
-});*/
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
@@ -29,3 +29,7 @@ app.post('/createItem', function (req, res) {
     console.log(req.body);
     createItem.create(req, res);
 });
+
+app.get('/getItemsByVoucherExpiry', function (req, res) {
+    getItemsByVoucherExpiry.find(req, res);
+ });
