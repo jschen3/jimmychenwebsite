067f514b21573ee5a84d27a18ed33bd677106c1e
kshah5 <kshah0@walmartlabs.com>
2018-03-01 11:03:20 -0800

    Adding basic models

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post('/createItem', function (req, res) {
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'))});
 
 app.get('/getItemsByVoucherExpiry', function (req, res) {
     getItemsByVoucherExpiry.find(req, res);
