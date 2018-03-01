1449052693bfb07c4e09c957e4a7a4a25cf76887
Sneha Ashtikar - s0a00mv <sashtikar@walmartlabs.com>
2018-03-01 15:07:27 -0800

    default route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ItemDetailComponent} from './item-detail/item-detail.component';
+import {CardsContainer} from './card/card.component';
 
 const routes: Routes = [
-  { path: 'item', component: ItemDetailComponent }
+  { path: 'item', component: ItemDetailComponent },
+  { path: '', component: CardsContainer, pathMatch: 'full' },
+  // { path: '', redirectTo: '/cards', pathMatch: 'full' },
 ];
 
 
