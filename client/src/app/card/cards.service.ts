8427b0fa2b2d0fc0f244e58facfcc45967afebc7
Sneha Ashtikar - s0a00mv <sashtikar@walmartlabs.com>
2018-03-01 17:31:21 -0800

    fix css for items carousel

--- client/src/app/card/cards.service.ts
+++ client/src/app/card/cards.service.ts
@@ -7,13 +7,14 @@ export class CardsService{
 		let items:Item[]=[
 			{
 			  "id": "464608521",
-			  "name": "Straight Talk, Silver",
+			  "name": "Straight Talk, Silver vhvgujbilhnik;vvgctcxhfjvhjj",
 			  "image_small": "https://i5.wal.co/asr/4c2f51a3-4b89-4382-b032-3951c4e51823_1.c96c7bf7a0b991268548ff3dd79b5290.jpeg?odnWidth=180&odnHeight=180&odnBg=ffffff",
 			  "image_big": "https://i5.walmartimages.com/asr/4c2f51a3-4b89-4382-b032-3951c4e51823_1.c96c7bf7a0b991268548ff3dd79b5290.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF",
 			  "description": "iPhone 8 is a new generation of iPhone. Designed with the most durable glass ever in a smartphone and a stronger aerospace grade aluminum band. Charges wirelessly. Resists water and dust. 4.7-inch Retina HD display with True Tone. 12MP camera with new sensor and advanced image signal processor. Powered by A11 Bionic, the most powerful and smartest chip ever in a smartphone. Supports augmented reality experiences in games and apps. With iPhone 8, intelligence has never looked better. 4.7-inch Retina HD display with True Tone3 All-glass and aluminum design, water and dust resistant2 12MP camera with OIS and 4K video up to 60 fps 7MP FaceTime HD camera with Retina Flash for stunning selfies4 Touch ID for secure authentication and Apple Pay A11 Bionic, the most powerful and smartest chip in a smartphone Wireless chargingworks with Qi chargers",
 			  "current_price": "699.00",
 			  "vouched_price": "649.00",
-			  "min_voucher": "100",
+				"min_voucher": "100",
+				"vouches":45,
 			  "is_vouchable": true,
 			  "voucher_expiration": "2018-03-23T00:00:00Z"
 			},
@@ -25,7 +26,8 @@ export class CardsService{
 			  "description": "iPhone 8 is a new generation of iPhone. Designed with the most durable glass ever in a smartphone and a stronger aerospace grade aluminum band. Charges wirelessly. Resists water and dust. 4.7-inch Retina HD display with True Tone. 12MP camera with new sensor and advanced image signal processor. Powered by A11 Bionic, the most powerful and smartest chip ever in a smartphone. Supports augmented reality experiences in games and apps. With iPhone 8, intelligence has never looked better. 4.7-inch Retina HD display with True Tone3 All-glass and aluminum design, water and dust resistant2 12MP camera with OIS and 4K video up to 60 fps 7MP FaceTime HD camera with Retina Flash for stunning selfies4 Touch ID for secure authentication and Apple Pay A11 Bionic, the most powerful and smartest chip in a smartphone Wireless chargingworks with Qi chargers",
 			  "current_price": "799.00",
 			  "vouched_price": "749.00",
-			  "min_voucher": "100",
+				"min_voucher": "100",
+				"vouches":45,
 			  "is_vouchable": true,
 			  "voucher_expiration": "2018-03-23T00:00:00Z"
 			},
@@ -37,7 +39,8 @@ export class CardsService{
 			  "description": "iPhone 8 is a new generation of iPhone. Designed with the most durable glass ever in a smartphone and a stronger aerospace grade aluminum band. Charges wirelessly. Resists water and dust. 4.7-inch Retina HD display with True Tone. 12MP camera with new sensor and advanced image signal processor. Powered by A11 Bionic, the most powerful and smartest chip ever in a smartphone. Supports augmented reality experiences in games and apps. With iPhone 8, intelligence has never looked better. 4.7-inch Retina HD display with True Tone3 All-glass and aluminum design, water and dust resistant2 12MP camera with OIS and 4K video up to 60 fps 7MP FaceTime HD camera with Retina Flash for stunning selfies4 Touch ID for secure authentication and Apple Pay A11 Bionic, the most powerful and smartest chip in a smartphone Wireless chargingworks with Qi chargers",
 			  "current_price": "799.00",
 			  "vouched_price": "749.00",
-			  "min_voucher": "100",
+				"min_voucher": "100",
+				"vouches":45,
 			  "is_vouchable": true,
 			  "voucher_expiration": "2018-03-23T00:00:00Z"
 			},{
@@ -48,7 +51,8 @@ export class CardsService{
 			  "description": "iPhone 8 is a new generation of iPhone. Designed with the most durable glass ever in a smartphone and a stronger aerospace grade aluminum band. Charges wirelessly. Resists water and dust. 4.7-inch Retina HD display with True Tone. 12MP camera with new sensor and advanced image signal processor. Powered by A11 Bionic, the most powerful and smartest chip ever in a smartphone. Supports augmented reality experiences in games and apps. With iPhone 8, intelligence has never looked better. 4.7-inch Retina HD display with True Tone3 All-glass and aluminum design, water and dust resistant2 12MP camera with OIS and 4K video up to 60 fps 7MP FaceTime HD camera with Retina Flash for stunning selfies4 Touch ID for secure authentication and Apple Pay A11 Bionic, the most powerful and smartest chip in a smartphone Wireless chargingworks with Qi chargers",
 			  "current_price": "799.00",
 			  "vouched_price": "749.00",
-			  "min_voucher": "100",
+				"min_voucher": "100",
+				"vouches":45,
 			  "is_vouchable": true,
 			  "voucher_expiration": "2018-03-23T00:00:00Z"
 			},{
@@ -59,7 +63,8 @@ export class CardsService{
 			  "description": "iPhone 8 is a new generation of iPhone. Designed with the most durable glass ever in a smartphone and a stronger aerospace grade aluminum band. Charges wirelessly. Resists water and dust. 4.7-inch Retina HD display with True Tone. 12MP camera with new sensor and advanced image signal processor. Powered by A11 Bionic, the most powerful and smartest chip ever in a smartphone. Supports augmented reality experiences in games and apps. With iPhone 8, intelligence has never looked better. 4.7-inch Retina HD display with True Tone3 All-glass and aluminum design, water and dust resistant2 12MP camera with OIS and 4K video up to 60 fps 7MP FaceTime HD camera with Retina Flash for stunning selfies4 Touch ID for secure authentication and Apple Pay A11 Bionic, the most powerful and smartest chip in a smartphone Wireless chargingworks with Qi chargers",
 			  "current_price": "799.00",
 			  "vouched_price": "749.00",
-			  "min_voucher": "100",
+				"min_voucher": "100",
+				"vouches":45,
 			  "is_vouchable": true,
 			  "voucher_expiration": "2018-03-23T00:00:00Z"
 			},{
@@ -70,7 +75,8 @@ export class CardsService{
 			  "description": "iPhone 8 is a new generation of iPhone. Designed with the most durable glass ever in a smartphone and a stronger aerospace grade aluminum band. Charges wirelessly. Resists water and dust. 4.7-inch Retina HD display with True Tone. 12MP camera with new sensor and advanced image signal processor. Powered by A11 Bionic, the most powerful and smartest chip ever in a smartphone. Supports augmented reality experiences in games and apps. With iPhone 8, intelligence has never looked better. 4.7-inch Retina HD display with True Tone3 All-glass and aluminum design, water and dust resistant2 12MP camera with OIS and 4K video up to 60 fps 7MP FaceTime HD camera with Retina Flash for stunning selfies4 Touch ID for secure authentication and Apple Pay A11 Bionic, the most powerful and smartest chip in a smartphone Wireless chargingworks with Qi chargers",
 			  "current_price": "799.00",
 			  "vouched_price": "749.00",
-			  "min_voucher": "100",
+				"min_voucher": "100",
+				"vouches":45,
 			  "is_vouchable": true,
 			  "voucher_expiration": "2018-03-23T00:00:00Z"
 			}
