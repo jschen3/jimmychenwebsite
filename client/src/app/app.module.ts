6d6a8c1c64c3ecccf451318b1e62b2894855b632
j0c02gr <jimmy.chen@walmartlabs.com>
2018-03-02 10:23:48 -0800

    adding checkout files

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
 import {CardsContainer} from './card/card.component';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { ItemService } from './item-detail/item.service';
-
+import {CheckoutComponent} from './checkout/checkout.component';
+import {FormsModule} from '@angular/forms'
 
 
 
@@ -17,13 +18,14 @@ import { ItemService } from './item-detail/item.service';
 @NgModule({
   declarations: [
     AppComponent, CardsContainer,
-    ItemDetailComponent
+    ItemDetailComponent,CheckoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    FormsModule
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [ItemService],
