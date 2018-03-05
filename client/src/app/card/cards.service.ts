3e42f3ddcdc6911de7602b755fa278bce21af976
Sneha Ashtikar - s0a00mv <sashtikar@walmartlabs.com>
2018-03-04 23:48:22 -0800

    fix days left on button

--- client/src/app/card/cards.service.ts
+++ client/src/app/card/cards.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Item} from '../../model/item'
 import {ItemService} from '../item-detail/item.service';
+import * as moment from 'moment';
 
 @Injectable()
 export class CardsService{
@@ -16,11 +17,16 @@ export class CardsService{
 				this.itemService.getItemById(item.id).subscribe(item=> {
 					console.log(item);
 					var today = new Date();
-					var daysLeft = Math.round((new Date(item.voucher_expiration).getTime() - today.getTime())/(60*60*24*1000));
-					if (daysLeft<=1){
+					var a = moment();
+					var b = moment(item.voucher_expiration);
+					let daysLeft = b.diff(a,  'days')
+					console.log('difference in date', daysLeft);
+					// var daysLeft = Math.round((new Date(item.voucher_expiration).getTime() - today.getTime())/(60*60*24*1000));
+					if (daysLeft<1){
+						item.days_left_text = "Expires Today";
+					}else if (daysLeft == 1){
 						item.days_left_text = "1 day left";
-					}
-					else{
+					}else {
 						item.days_left_text = daysLeft + " days left";
 					}
 					self.arr.push(item);
